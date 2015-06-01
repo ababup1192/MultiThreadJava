f0bbd38a5e32e3c822bc149c07bb9ae48a4971a4
ababup1192 <ababup1192@gmail.com>
2015-06-01 14:53:35 +0900

    First commit

---
+++ src/main/java/org/ababup1192/Main.java
@@ -0,0 +1,12 @@
+package org.ababup1192;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10000; i++) {
+            System.out.print("Good!");
+        }
+    }
+
+}
