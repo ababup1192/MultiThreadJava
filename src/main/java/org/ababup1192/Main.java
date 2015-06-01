23b3b8e5c9c9045ed50fa8cd2cd1b1308a9b77c9
ababup1192 <ababup1192@gmail.com>
2015-06-01 16:34:56 +0900

    Using ThreadFactory

--- src/main/java/org/ababup1192/Main.java
+++ src/main/java/org/ababup1192/Main.java
@@ -1,12 +1,14 @@
 package org.ababup1192;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        new Thread(new Printer("foo")).start();
-        new Thread(new Printer("bar")).start();
-
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        threadFactory.newThread(new Printer("foo")).start();
+        threadFactory.newThread(new Printer("bar")).start();
     }
 
 }
