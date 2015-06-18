b96ece6ba8a6e627c33b32dfaaab50640a941b41
ababup1192 <ababup1192@gmail.com>
2015-06-18 14:31:14 +0900

    Implement Balking pattern

--- src/main/java/org/ababup1192/Main.java
+++ src/main/java/org/ababup1192/Main.java
@@ -3,9 +3,9 @@ package org.ababup1192;
 public class Main {
 
     public static void main(String[] args) {
-        RequestQueue requestQueue = new RequestQueue();
-        new Client(requestQueue, "Alice", 3141592L).start();
-        new Server(requestQueue, "Bobby", 6535897L).start();
+        Data data = new Data("data.txt", "(empty)");
+        new Changer("Changer", data).start();
+        new Saver("Saver", data).start();
     }
 
 }
