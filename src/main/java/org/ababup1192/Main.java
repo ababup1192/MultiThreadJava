c82fb4ccca16d21e26f5b788ae67333a8200def7
ababup1192 <ababup1192@gmail.com>
2015-07-18 22:49:47 +0900

    Implements Worker Thread pattern

--- src/main/java/org/ababup1192/Main.java
+++ src/main/java/org/ababup1192/Main.java
@@ -3,12 +3,11 @@ package org.ababup1192;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("main BEGIN");
-        Host host = new Host();
-        host.request(10, 'A');
-        host.request(20, 'B');
-        host.request(30, 'C');
-        System.out.println("main END");
+        Channel channel = new Channel(5);
+        channel.startWorkers();
+        new ClientThread("Alice", channel).start();
+        new ClientThread("Bobby", channel).start();
+        new ClientThread("Chris", channel).start();
     }
 
 }
---
+++ src/main/java/org/ababup1192/WorkerThread.java
@@ -0,0 +1,17 @@
+package org.ababup1192;
+
+
+public class WorkerThread extends Thread{
+    public final Channel channel;
+    public WorkerThread(String name, Channel channel){
+        super(name);
+        this.channel = channel;
+    }
+
+    public void run(){
+        while(true){
+            Request request = channel.takeRequest();
+            request.execute();
+        }
+    }
+}
