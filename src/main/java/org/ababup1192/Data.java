ed8d325084317bc607b91a342580f2eab94d7447
ababup1192 <ababup1192@gmail.com>
2015-07-18 16:17:21 +0900

    Implements Read Write Lock pattern

---
+++ src/main/java/org/ababup1192/Data.java
@@ -0,0 +1,55 @@
+package org.ababup1192;
+
+
+public class Data {
+    private final char[] buffer;
+    private final ReadWriteLock lock = new ReadWriteLock();
+
+    public Data(int size) {
+        this.buffer = new char[size];
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = '*';
+        }
+    }
+
+    public char[] read() throws InterruptedException {
+        lock.readLock();
+        try {
+            return doRead();
+        } finally {
+            lock.readUnlock();
+        }
+    }
+
+    public void write(char c) throws InterruptedException {
+        lock.writeLock();
+        try {
+            doWrite(c);
+        } finally {
+            lock.writeUnlock();
+        }
+    }
+
+    private char[] doRead() {
+        char[] newBuf = new char[buffer.length];
+        System.arraycopy(buffer, 0, newBuf, 0, buffer.length);
+        slowly();
+        return newBuf;
+    }
+
+    private void doWrite(char c) {
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = c;
+            slowly();
+        }
+    }
+
+    private void slowly() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/org/ababup1192/Main.java
+++ src/main/java/org/ababup1192/Main.java
@@ -3,13 +3,15 @@ package org.ababup1192;
 public class Main {
 
     public static void main(String[] args) {
-        Table table = new Table(3);
-        new Maker("MakerThread-1", table, 31415).start();
-        new Maker("MakerThread-2", table, 92633).start();
-        new Maker("MakerThread-3", table, 58979).start();
-        new Eater("EaterThread-1", table, 32384).start();
-        new Eater("EaterThread-2", table, 62643).start();
-        new Eater("EaterThread-3", table, 38327).start();
+        Data data = new Data(10);
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new ReaderThread(data).start();
+        new WriterThread(data, "ABCDEFGHIJKLMNOPQRSTUVWXYZ").start();
+        new WriterThread(data, "abcdefghijklmnopqrstuvwxyz").start();
     }
 
 }
